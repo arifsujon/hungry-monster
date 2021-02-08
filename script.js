2aa4f4ea8d52a382cc7e613f04dd2c8522472c27
Ariful Islalm <arifsujon.bd@gmail.com>
2021-02-08 23:18:15 +0600

    update single meal part

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-
+//Search Meal
+const mealItems = document.getElementById("meals");
 function searchMeal() {
     const inputMeal = document.getElementById("inputMeal").value;
-    const mealItems = document.getElementById("meals");
+    
 
     const api = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputMeal}`;
     fetch(api)
         .then(res => res.json())
         .then(data => {
             mealItems.innerHTML = data.meals.map((meal) => `
-            <div onclick="displayCountryDetail('${meals.strMeal}')" class="meal">
+            <div onclick="displayMealDetail(${meal.idMeal})" class="meal">
                 <img src="${meal.strMealThumb}"/>
                 <h6>${meal.strMeal}</h6>
             </div>`
@@ -16,21 +17,31 @@ function searchMeal() {
         })
 }
 
-
-
-
-
-
-const displayCountryDetail = mealID => {
-    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`
+// Display Single Meal Info
+const displayMealDetail = (idMeal) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
     fetch(url)
     .then(res => res.json())
-    .then(data => renderCountryInfo(data[1]));
+    .then(data =>  renderMealInfo(data.meals[0]));
 }
-const renderCountryInfo = meals => {
-    const countryDiv = document.getElementById('countryDetail');
-    countryDiv.innerHTML = `
+
+const renderMealInfo = meals => {
+    console.log(meals);
+    const mealDiv = document.getElementById('mealDetail');
+    mealDiv.innerHTML = `
+        <img src="${meals.strMealThumb}">
         <h1>${meals.strMeal}</h1>
+        <h5>Ingredients</h5>
+        <p>${meals.strIngredient1}</p>
+        <p>${meals.strIngredient2}</p>
+        <p>${meals.strIngredient3}</p>
+        <p>${meals.strIngredient4}</p>
+        <p>${meals.strIngredient5}</p>
+        <p>${meals.strIngredient6}</p>
+        <p>${meals.strIngredient7}</p>
+        <p>${meals.strIngredient8}</p>
+        <p>${meals.strIngredient9}</p>
+        <p>${meals.strIngredient10}</p>
        
     `
 }
